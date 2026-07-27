6ee9a6733d3dcf7a146fd2320a8164d28e9d4ec9
joao-anthonyo <joaoanthonyo0655@gmail.com>
2026-07-27 15:13:54 -0300

    Refactor Personagem class constructor and methods
    Refactor Personagem class methods and formatting.

--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,27 @@
-public class Personagem {
+public class Personagem{
+    // atributos = variaveis
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase){
+        // contrutor 
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
-    public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+    public void exibirInformacoes(){
+        // informacoes que apareceram no metodo exibirInformacoes no main
+        System.out.println(
+        "nome: " + nome + "\n" +
+        " classe: " + classe + "\n" +
+        " nivel: " + nivel + "\n" +
+        " pontos de vida: " + pontosDeVida + "\n" +
+        " pode base " + poderBase);
     }
 }
-
